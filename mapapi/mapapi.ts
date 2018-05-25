17dbdcd7cd0d64caaf4b4f372966fe711af0dcc9
pamelagao <pamela.gao@hotmail.com>
2018-05-25 14:10:16 -0400

    lab3 part two

--- mapapi/mapapi.ts
+++ mapapi/mapapi.ts
@@ -1,5 +1,5 @@
 let map : any;
-let addresses : any[];
+let addresses : any[] = [];
 let mapMarkers : MapMarker[] = [];
 
 // interface
@@ -12,6 +12,7 @@ let Toronto : LatLng = { lat : 43, lng : -79.38 };
 
 // class for map markers
 class MapMarker {
+
     Address : string;
     LatLng : LatLng;
 
@@ -20,15 +21,88 @@ class MapMarker {
     }
 }
 
+$.ajax({
+    url : './PublicPlaces.json',
+    dataType : 'json',
+    success : function (data) {
+
+        addresses = data;
+
+        for (let i of addresses) {
+            let newMapMarker : MapMarker = new MapMarker(i.address);
+            mapMarkers.push(newMapMarker);   
+        }
+        // console.log(mapMarkers);
+    }
+});
+
+let markerIndex : number = 0;
+
+// set laglng to 'mapMarkers' object
+function setLatLng() : void {
+    mapMarkers[markerIndex].LatLng = getLatLng(mapMarkers[markerIndex].Address);
+}
+
+window.onload = function () {
+
+    // for (let i = 0; i < 10 ; i++) {
+    for (let i = 0; i < 50; i++) {
+
+        getLatLng(mapMarkers[i]);
+
+        setTimeout( function(){ 
+        
+            getMarker(mapMarkers[i].LatLng);
+            
+        }, 500); 
+    }
+}
+
 function initMap () {
 
-    let geocoder : object = new google.maps.Geocoder;
-
     map = new google.maps.Map(
+
         document.getElementById('map'), 
         { 
             center: Toronto,
             zoom: 8
         }
     );
+}
+
+function getLatLng (map : MapMarker) : LatLng {
+        
+    let geocoder : object = new google.maps.Geocoder;
+
+    geocoder.geocode(
+        {
+            'address' : map.Address
+        },
+        function (results, status) {
+
+            if (status === 'OK') {
+
+                map.LatLng = {
+                    lat : results[0].geometry.location.lat(),
+                    lng : results[0].geometry.location.lng()
+                }
+
+                getMarker(map.LatLng);
+
+            } else {
+
+                setTimeout( function(){ getLatLng(map) } , 200);
+                // setInterval( getLatLng(map), 200);
+            }
+        }
+    );
+    return map.LatLng;
+}
+
+function getMarker (marker : LatLng) {
+
+    let marker = new google.maps.Marker({
+        position: marker,
+        map: map
+    });
 }
